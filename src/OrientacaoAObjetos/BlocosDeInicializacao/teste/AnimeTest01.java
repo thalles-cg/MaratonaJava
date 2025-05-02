3de37f1ec89659c3d817c1aa44e9b77cf13c9ada
thalles-cg <thalles.pity@gmail.com>
2025-05-02 10:46:28 -0300

    feature: Aula de Blocos de Inicializacao

---
+++ src/OrientacaoAObjetos/BlocosDeInicializacao/teste/AnimeTest01.java
@@ -0,0 +1,8 @@
+package src.OrientacaoAObjetos.BlocosDeInicializacao.teste;
+
+import src.OrientacaoAObjetos.BlocosDeInicializacao.domain.Anime;
+
+public class AnimeTest01 {
+
+    Anime anime = new Anime();
+}
