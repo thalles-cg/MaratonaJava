23bd3203768419db2beb5eb66016eb40d7645be0
thalles-cg <thalles.pity@gmail.com>
2025-05-02 21:14:12 -0300

    feature: Aulas de Entrade de Dados

--- src/OrientacaoAObjetos/ModificadorEstatico/test/CarroTest01.java
+++ src/OrientacaoAObjetos/ModificadorEstatico/test/CarroTest01.java
@@ -1,10 +1,10 @@
 package src.OrientacaoAObjetos.ModificadorEstatico.test;
 
-<<<<<<< HEAD
+
 import src.OrientacaoAObjetos.ModificadorEstatico.dominio.Carro;
-=======
-import src.OrientacaoAObjetos.ModificadorEstatico.domain.Carro;
->>>>>>> 8235e11d2ec5394417181cb3dffda87853147705
+
+
+
 
 public class CarroTest01 {
     public static void main(String[] args) {
