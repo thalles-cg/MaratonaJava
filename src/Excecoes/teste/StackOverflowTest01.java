f5391d3458c8c72168cd31ce700a2b35a068b6f9
thalles-cg <thalles.pity@gmail.com>
2025-05-12 06:41:01 -0300

    feature: Aulas de Exceções

---
+++ src/Excecoes/teste/StackOverflowTest01.java
@@ -0,0 +1,12 @@
+package src.Excecoes.teste;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+        // Uma stack é uma sequência de chamadas na memória. Um stackoverflow acontece quando essa chamada acontece tantas vezes, que acaba o espaço de memória de uma stack.
+        // O stackoverflow é um erro. Erros são falhas no sistema que não são recuperáveis, que quando acontecessem há a necessidade de reiniciar o programa.
+    }
+    public static void recursividade(){
+        recursividade();
+    }
+}
