b34045f37236575c0c7e146451500564fc27fe4e
thalles-cg <thalles.pity@gmail.com>
2025-06-03 12:50:12 -0300

    feat: Aulas de Wrapper

---
+++ src/Classes_Utilitarias/Wrapper/teste/WrapperTest01.java
@@ -0,0 +1,28 @@
+package src.Classes_Utilitarias.Wrapper.teste;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+
+        // Primitivos
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 10;
+        long longP = 10; // Não precisa pôr o L no final do long quando ele é um tipo primitivo
+        float floatP = 10; // Mesma coisa para o float
+        double doubleP = 10;
+        char charP = 'P';
+        boolean booleanP = true;
+
+        // Wrappers
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 10;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+
+    }
+}
