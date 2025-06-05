a55cc15eb37a2c933ed9ccf138d9289fd2a38c92
thalles-cg <thalles.pity@gmail.com>
2025-06-05 16:43:27 -0300

    feat: Aulas de LocalDate

---
+++ src/Classes_Utilitarias/Datas/teste/LocalDateTest01.java
@@ -0,0 +1,17 @@
+package src.Classes_Utilitarias.Datas.teste;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+//        System.out.println(new Date());
+//        System.out.println(Calendar.getInstance());
+
+        LocalDate date = LocalDate.of(2025, Month.JUNE, 20);
+
+
+    }
+}
