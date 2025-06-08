a1644fc6717f62fab15c64de6685f6c2f83966a8
thalles-cg <thalles.pity@gmail.com>
2025-06-08 18:08:00 -0300

    feat: Aulas de Period

--- src/Classes_Utilitarias/Datas/teste/PeriodTest01.java
+++ src/Classes_Utilitarias/Datas/teste/PeriodTest01.java
@@ -6,9 +6,10 @@ import java.time.Period;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2);
-        Period p1 = Period.between()
-
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2);
+        Period p1 = Period.between(now, nowAfterTwoYears);
+        System.out.println(p1);
+        Period.of()
     }
 }
