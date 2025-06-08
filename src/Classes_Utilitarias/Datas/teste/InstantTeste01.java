74edcd3559c54b5b7179935f21aa1dd18827ac01
thalles-cg <thalles.pity@gmail.com>
2025-06-08 17:40:01 -0300

    feat: Aulas de Instant

--- src/Classes_Utilitarias/Datas/teste/InstantTeste01.java
+++ src/Classes_Utilitarias/Datas/teste/InstantTeste01.java
@@ -1,4 +1,15 @@
 package src.Classes_Utilitarias.Datas.teste;
 
+import java.time.Instant;
+
 public class InstantTeste01 {
+    public static void main(String[] args) {
+
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.getNano());
+        System.out.println(Instant.ofEpochSecond(1999999999));
+
+    }
 }
