9f26b0f907a0872c41315f21609c6c599ba7c864
thalles-cg <thalles.pity@gmail.com>
2025-06-08 17:39:28 -0300

    feat: Aulas de LocalDateTime

--- src/Classes_Utilitarias/Datas/teste/LocalDateTime01.java
+++ src/Classes_Utilitarias/Datas/teste/LocalDateTime01.java
@@ -1,4 +1,21 @@
 package src.Classes_Utilitarias.Datas.teste;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class LocalDateTime01 {
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+
+        LocalDate date = LocalDate.parse("2024-02-12");
+        LocalTime time = LocalTime.of(12, 23, 45);
+
+        LocalDateTime dataHora = date.atTime(time);
+
+        System.out.println(dataHora);
+    }
 }
