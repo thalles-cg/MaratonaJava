2cb3a2ce28fe6fb4261d97f8b911e0c0700a0094
thalles-cg <thalles.pity@gmail.com>
2025-06-18 12:45:35 -0300

    feat: Exercício Count Odd Numbers CodeWars

---
+++ Exercises/codewars/CountOddNumbers/Kata.java
@@ -0,0 +1,16 @@
+package Exercises.codewars.CountOddNumbers;
+
+import java.util.*;
+
+public class Kata {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int oddCount(int n){
+
+        return (int)(Math.ceil(((n-1)/2d)));
+    }
+
+}
