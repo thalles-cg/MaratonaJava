8c04cc3ed10ea398e797c959b6a1befcd6e517c3
thalles-cg <thalles.pity@gmail.com>
2025-06-17 23:15:04 -0300

    feat: Exercício Sum The Strings CodeWars

--- Exercises/codewars/SumTheStrings/Kata.java
+++ Exercises/codewars/SumTheStrings/Kata.java
@@ -1,4 +1,16 @@
 package Exercises.codewars.SumTheStrings;
 
-public class Kata {
+public class Kata{
+    public static String sumStr(String a, String b){
+        int num1 = 0;
+        int num2 = 0;
+
+        if (!a.isEmpty()){
+            num1 = Integer.parseInt(a);
+        }
+        if (!b.isEmpty()){
+            num2 = Integer.parseInt(b);
+        }
+        return String.format("%d", (num1 + num2));
+    }
 }
