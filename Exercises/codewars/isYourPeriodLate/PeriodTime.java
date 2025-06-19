a73f05cb5f74c02b665f948e1ddd96f0d12fcf08
thalles-cg <thalles.pity@gmail.com>
2025-06-19 12:24:07 -0300

    feat: Exercício Is Your Period Late CodeWars

---
+++ Exercises/codewars/isYourPeriodLate/PeriodTime.java
@@ -0,0 +1,17 @@
+package Exercises.codewars.isYourPeriodLate;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class PeriodTime {
+
+    public static void main(String[] args) {
+
+        System.out.println(PeriodTime.periodIsLate(LocalDate.of(2016, 6, 13), LocalDate.of(2016, 7, 16), 35));
+
+    }
+
+    public static boolean periodIsLate(LocalDate last,LocalDate today,int cycleLength) {
+        return ChronoUnit.DAYS.between(last, today) > cycleLength;
+    }
+}
