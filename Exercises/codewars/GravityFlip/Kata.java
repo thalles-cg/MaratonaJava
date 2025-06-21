9bd47913653e6ec31cd9fb91f1c8ba2b3a69fb0e
thalles-cg <thalles.pity@gmail.com>
2025-06-21 17:24:34 -0300

    feat: Exercício Gravity Flip CodeWars

--- Exercises/codewars/GravityFlip/Kata.java
+++ Exercises/codewars/GravityFlip/Kata.java
@@ -1,4 +1,26 @@
 package Exercises.codewars.GravityFlip;
 
+import java.util.Arrays;
+
 public class Kata {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(Kata.flip('R', new int[]{ 3,2,1,2 })));
+        System.out.println(Arrays.toString(Kata.flip('L', new int[]{ 1,4,5,3,5 })));
+
+
+    }
+
+    public static int[] flip(char dir, int[] arr) {
+        Arrays.sort(arr);
+        if(dir == 'R'){
+            return arr;
+        }
+        for (int i = 0; i < arr.length/2 ; i++){
+            int temp = arr[i];
+            arr[i] = arr[arr.length -1 -i];
+            arr[arr.length -1 -i] = temp;
+        }
+        return arr;
+    }
 }
