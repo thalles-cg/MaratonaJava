5e8bb689ca87a22e68450d4f4b5060aee5025d93
thalles-cg <thalles.pity@gmail.com>
2025-06-25 14:39:39 -0300

    feat: Exercício Find Maximum and Minimum Values of a List CodeWars

---
+++ Exercises/codewars/FindMinAndMaxValue/Kata.java
@@ -0,0 +1,14 @@
+package Exercises.codewars.FindMinAndMaxValue;
+
+import java.util.Arrays;
+
+public class Kata {
+
+    public int min(int[] list) {
+        return Arrays.stream(list).min().getAsInt();
+    }
+
+    public int max(int[] list) {
+        return Arrays.stream(list).max().getAsInt();
+    }
+}
