da0a77fc807a7f61adff3e1cbe54a1143578b6c1
thalles-cg <thalles.pity@gmail.com>
2025-06-28 14:03:46 -0300

    feat: Exercício Sort my Textbooks CodeWars

--- Exercises/codewars/SortMyTextbooks/sorter.java
+++ Exercises/codewars/SortMyTextbooks/sorter.java
@@ -2,11 +2,12 @@ package Exercises.codewars.SortMyTextbooks;
 
 import java.util.*;
 
-import java.util.*;
-
 class sorter {
 
     public static List<String> sort(List<String> textbooks) {
-        return textbooks.stream().sorted().toList();
+        if (textbooks == null) return null;
+        List<String> sorted = new ArrayList<>(textbooks);
+        sorted.sort(String.CASE_INSENSITIVE_ORDER);
+        return sorted;
     }
 }
